13fa01798bfee24bb5d15e4a88c5cb4409409260
anusonIns <jejeng33@hotmail.com>
2020-09-23 14:15:30 +0700

    ok
    esdfsf

---
+++ App_Code/operationDB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for operationDB
+/// </summary>
+public class operationDB
+{
+    public string OperationMessage { get; set; }
+    public operationDB()
+    {
+        transec_datasetDataContext trans_db = new transec_datasetDataContext();
+        var customers = trans_db.ExecuteQuery<TransectionDB>(@"SELECT  * FROM   TransectionDB ");
+
+        foreach (var c in customers)
+        {
+        }
+    }
+    public bool InsertTransection(TransectionDB tb)
+    {
+        try
+        {
+            using (transec_datasetDataContext dc = new transec_datasetDataContext())
+            {
+                TransectionDB objEmp = new TransectionDB();
+                objEmp.Id = tb.Id;
+                objEmp.Transaction_Id = tb.Transaction_Id;
+                objEmp.Amount = tb.Amount;
+                objEmp.Currency_Code = tb.Currency_Code;
+                objEmp.Transaction_Date =tb.Transaction_Date;
+                objEmp.Status = tb.Status;
+                dc.TransectionDBs.InsertOnSubmit(objEmp);
+                dc.SubmitChanges();
+            }
+            OperationMessage = "Inserted.";
+            return true;
+        }
+        catch(Exception ex)
+        {
+            OperationMessage = ex.Message;
+            return false;
+        }
+    }
+}
---
+++ App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TransectionDB]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Transaction_Id] VARCHAR(50) NOT NULL, 
+    [Amount] DECIMAL(16, 2) NOT NULL, 
+    [Currency_Code] NVARCHAR(8) NOT NULL, 
+    [Transaction_Date] DATETIME NOT NULL, 
+    [Status] VARCHAR(12) NOT NULL
+)
